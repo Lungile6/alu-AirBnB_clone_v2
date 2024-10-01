6eff6806c884310a8a53d72ca2fc52733b7fd195
Lungile6 <155407787+Lungile6@users.noreply.github.com>
2024-10-01 13:17:16 +0200

    Update_setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
-nstall Nginx if not already installed
+# The Script setup web servers for the deployment of web-static
+
+# Install Nginx if not already installed
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
-# create folders
+# Create necessary folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-#create a fake html file
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-# create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# give ownership
+# Create a fake HTML file
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link (remove if exists and recreate)
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership
 sudo chown -hR ubuntu:ubuntu /data/
+
+# Update Nginx configuration
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+
+# Restart Nginx
+sudo service nginx restart
